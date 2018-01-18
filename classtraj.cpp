5860d4be33f3e8cb8ea7c36d709a918d6e70504b
Sean <smann95@hotmail.com>
2018-01-18 15:05:08 -0500

    basic harmonic oscillator stuff

--- classtraj.cpp
+++ classtraj.cpp
@@ -2,15 +2,43 @@
 #include <cmath>
 #include <sstream>
 #include <iomanip>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
-  if (argc < 6){
-    cout << error << endl;}
+  if (argc < 2){
+    cout << "error" << endl;
+    return 1;
+  }
+  vector<double> constants;
+  double temp;
+  ifstream input (argv[1]);
+  while (input >> temp){
+    constants.push_back(temp);
+    cout << temp << endl;
+  }
+ double maximum_time = constants[0];
+ double initial_position = constants[1];
+ double initial_velocity = constants[2];
+ double angular_frequency = constants[3];
+ double number_measurements = constants[4];
+ double time_step = maximum_time/number_measurements;
+double mass = 1;
 
-  stringstream ss_1(argv[1]);
-    if (!(ss_1 >>clear initial_position))
-      cout << initial_position;
+ double x0 = initial_position,
+        v0 = initial_velocity,
+        w = angular_frequency,
+        m = mass;
+ for (double time = 0; time <= maximum_time; time += time_step){
+   double position = x0*cos(w*time) + (v0/w)*sin(w*time);
+   double velocity = -w*x0*sin(w*time) + v0*cos(w*time);
+   double acceleration = -w*w*x0*sin(w*time) - v0*w*cos(w*time);
+   double kinetic_energy = 0.5*m*velocity*velocity;
+   double potential_energy = 0.5*m*w*w*position*position;
+   double total_energy = kinetic_energy + potential_energy;
+   cout << total_energy << endl;
+ }
   return 0;
 }
