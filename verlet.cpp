5f7f57a7525db6a5cd7b82369ddff640446a5c02
Sean <smann95@hotmail.com>
2018-01-22 05:29:39 -0500

    harmonic oscillator for verlet algorithm

--- verlet.cpp
+++ verlet.cpp
@@ -19,26 +19,28 @@ int main(int argc, char **argv)
     constants.push_back(temp);
   }
   double maximum_time = constants[0];
-  double initial_position = constants[1];
-  double initial_velocity = constants[2];
+  double position = constants[1];
+  double velocity = constants[2];
   double angular_frequency = constants[3];
   double number_measurements = constants[4];
   double time_step = maximum_time/number_measurements;
   double mass = 1;
-
-  double x0 = initial_position,
-         v0 = initial_velocity,
+  double x = position,
+         v = velocity,
          w = angular_frequency,
+         dt = time_step,
          m = mass;
-  for (double time = 0; time <= maximum_time; time += time_step){
-    double position = x0*cos(w*time) + (v0/w)*sin(w*time);
-    double velocity = -w*x0*sin(w*time) + v0*cos(w*time);
-    double acceleration = -w*w*x0*sin(w*time) - v0*w*cos(w*time);
+  double acceleration = -w*w*x;
+  double a = acceleration;
+  for (double time = 0; time <= maximum_time; time += dt){
+    v = v + 0.5*a*dt;
+    x = x + v*dt;
+    a = -w*w*x;
+    v = v + 0.5*a*dt;
     double kinetic_energy = 0.5*m*velocity*velocity;
     double potential_energy = 0.5*m*w*w*position*position;
     double total_energy = kinetic_energy + potential_energy;
-    cout << time << "\t\t" << position << "\t\t" << acceleration << "\t\t"
-      << total_energy <<  endl;
+    cout << time << "\t\t" << x << "\t\t" << total_energy << endl; 
   }
   return 0;
 }
